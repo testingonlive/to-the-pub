8f6e5d47a1819a3283d99bd8e4b0cc3e7e8f7403
jonny5 <testingonlive@gmail.com>
2015-01-28 10:25:29 +0000

    promises, promises

---
+++ js/promises.js
@@ -0,0 +1,47 @@
+module.export = {
+    
+    promiseScript: function( url ) {
+    
+        // return a new promise
+        return new Promise(function( resolve, reject ){
+
+            // create the script element
+            var scriptElm = document.createElement( 'script' );
+            scriptElm.type = 'text/javascript';
+            scriptElm.async = true;
+
+            // reject onerror
+            scriptElm.onerror = function( err ){
+                reject( err );
+            }
+
+            // resolve onload
+           scriptElm.onload = function(){
+               resolve();
+           }
+
+           // do the stuff to actually get the script
+           document.head.appendChild( scriptElm );
+           scriptElm.src = url;
+
+        });
+
+    },
+    
+    promiseGeoLocn: function() {
+    
+        // return a new promise
+        return new Promise(function( resolve, reject ){
+
+            // reject if geolocation isn't available
+            if ( !navigator.geolocation ) reject( 'geolocation API not available' );
+
+            // try and grab the current position
+            navigator.geolocation.getCurrentPosition( resolve, reject );
+        })
+    }
+    
+}
+
+
+
