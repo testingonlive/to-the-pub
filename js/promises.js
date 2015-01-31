b5af5504189b086d95230801c445796997551862
jonny5 <testingonlive@gmail.com>
2015-01-31 20:19:37 +0000

    add more promises

--- js/promises.js
+++ js/promises.js
@@ -1,48 +1,136 @@
-// export promise module 
-module.export = {
+function promiseGeoLocn() {
+
+    // return a new promise
+    return new Promise(function( resolve, reject ){
+
+        // reject if geolocation isn't available
+        if ( !navigator.geolocation ) reject( 'geolocation API not available' );
+
+        // try and grab the current position
+        navigator.geolocation.getCurrentPosition( resolve, reject );
+    })
+}
+
+
+function promisePubs( map ) {
     
-    promiseScript: function( url ) {
+    // return new promise
+    return new Promise(function( resolve, reject ){
+        
+        // set up request object
+        var _request = {
+            location: map.getCenter(),             
+            keyword: 'pub',
+            rankBy: google.maps.places.RankBy.DISTANCE
+        }
+        
+        new google.maps.places.PlacesService( map ).nearbySearch( _request, function( res, status ){
+            
+            // resolve if status equals ok
+            if (  status === google.maps.places.PlacesServiceStatus.OK ) {
+                resolve( res );     
+            } else {
+                reject( 'places service is not OK' );
+            }            
+        
+        });
+    });
+}
+
+
+
+function promisePubDetails( id, map ) {
     
-        // return a new promise
-        return new Promise(function( resolve, reject ){
-
-            // create the script element
-            var scriptElm = document.createElement( 'script' );
-            scriptElm.type = 'text/javascript';
-            scriptElm.async = true;
-
-            // reject onerror
-            scriptElm.onerror = function( err ){
-                reject( err );
-            }
-
-            // resolve onload
-           scriptElm.onload = function(){
-               resolve();
-           }
-
-           // do the stuff to actually get the script
-           document.head.appendChild( scriptElm );
-           scriptElm.src = url;
-
+    // return new promise
+    return new Promise(function( resolve, reject ){
+         
+        new google.maps.places.PlacesService( map ).getDetails( { placeId: id }, function( details, status ){
+            
+            // resolve if status equals ok
+            if (  status === google.maps.places.PlacesServiceStatus.OK ) {
+                resolve( details );     
+            } else {
+                reject( 'places service is not OK' );
+            }  
         });
 
-    },
-    
-    promiseGeoLocn: function() {
-    
-        // return a new promise
-        return new Promise(function( resolve, reject ){
-
-            // reject if geolocation isn't available
-            if ( !navigator.geolocation ) reject( 'geolocation API not available' );
-
-            // try and grab the current position
-            navigator.geolocation.getCurrentPosition( resolve, reject );
-        })
-    }
+        
+        
+    });
     
 }
+
+
+/*
+ * 
+ * get things going
+ * 
+ * using asns code which requires a function to be called
+ * 
+ */
+function mapsInit(){
+
+    // set map in function scope.
+    var map;
+    
+    
+    promiseGeoLocn()
+
+        .then(function( latLng ){
+            // set the map options from the return geo data
+            var _mapOpt = {
+                    center: { lat: latLng.coords.latitude, lng: latLng.coords.longitude },
+                    zoom: 17
+                };  
+
+            // create the map
+            map = new google.maps.Map( document.querySelector( '.mapCanvas' ), _mapOpt );
+
+            // return the pubs promise
+            return promisePubs( map )
+        })
+
+        .then( function( pubs ){
+            var info = new google.maps.InfoWindow();
+            // loop through the pubs array
+            // just replicating old behaviour for now
+            pubs.forEach( function( pub ){
+                var placeLoc = pub.geometry.location;
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: pub.geometry.location
+                });
+
+                google.maps.event.addListener( marker, 'click', function(){
+                    promisePubDetails( pub.place_id, map ).then(function( data ){
+                        console.log( data );
+                        window.test = data;
+                    })
+                })
+            })
+
+        })
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
 
 
 
